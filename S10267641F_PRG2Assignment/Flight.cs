2ff93f193377f800bd54ac9ea11b6129b7f14f89
Si Jia Kwoh <s10267641@connect.np.edu.sg>
2025-01-13 13:06:17 +0800

    Editing flight.cs

---
+++ S10267641F_PRG2Assignment/Flight.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10267641F_PRG2Assignment
+{
+    class Flight
+    {
+        public string FlightNumber {  get; set; }   
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status {  get; set; }
+
+        public Flight(string flightNumber, string origin, string destination, DateTime expectedTime, string status)
+        {
+            FlightNumber = flightNumber;
+            Origin = origin;
+            Destination = destination;
+            ExpectedTime = expectedTime;
+            Status = status;
+        }
+        public double CalculateFees()
+        {
+            
+        }
+        public override string ToString()
+        {
+            return;
+        }
+    }
+}
