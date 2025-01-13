fc63e8d568aedcdd7311b9399dd504518b2270eb
Jun Yang Ian Tan <s10268190@connect.np.edu.sg>
2025-01-13 15:43:29 +0800

    terminal

--- S10267641F_PRG2Assignment/Terminal.cs
+++ S10267641F_PRG2Assignment/Terminal.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace S10267641F_PRG2Assignment
 {
-    internal class Terminal
+    class Terminal
     {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airline { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+        public Dictionary<string, double> GateFees { get; set; }
+
+        public Terminal(string terminalName)
+        {
+            TerminalName = terminalName;
+            Airline = new Dictionary<string, Airline>();
+            Flights = new Dictionary<string, Flight>();
+            BoardingGates = new Dictionary<string, BoardingGate>();
+            GateFees = new Dictionary<string, double>();
+        }
+        public bool AddAirline(Airline airline)
+        {
+
+        }
+
+        public bool AddBoardingGate(BoardingGate boardingGate)
+        {
+
+        }
+
+        public Airline GetAirlineFromFlight(Flight flight)
+        {
+
+        }
+
+        public void PrintAirlineFees()
+        {
+
+        }
+
+        public override string ToString()
+        {
+
+        }
     }
 }
