79fd6d125524eba638b76063206262b9ffdf8fc7
Si Jia Kwoh <s10267641@connect.np.edu.sg>
2025-01-13 15:13:38 +0800

    subclasses

--- S10267641F_PRG2Assignment/NORMFlight.cs
+++ S10267641F_PRG2Assignment/NORMFlight.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace S10267641F_PRG2Assignment
 {
-    class NORMFlight
+    class NORMFlight : Flight
     {
+        public NORMFlight(string flightNumber, string origin, string destination, DateTime expectedTime, string status = "On Time")
+        : base(flightNumber, origin, destination, expectedTime, status)
+        { }
+
+        public override double CalculateFees()
+        {
+            return;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $", Fees: {CalculateFees()}";
+        }
     }
 }
