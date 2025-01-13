aea389ea73f6ce847ede9439ec391816607bbbd2
Si Jia Kwoh <s10267641@connect.np.edu.sg>
2025-01-13 22:55:37 +0800

    Airline Class

--- S10267641F_PRG2Assignment/Airline.cs
+++ S10267641F_PRG2Assignment/Airline.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace S10267641F_PRG2Assignment
 {
-    internal class Airline
+    class Airline
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Airline(string name, string code)
+        {
+            Name = name;
+            Code = code;
+            Flights = new Dictionary<string, Flight>();
+        }
+
+        public bool AddFlight(Flight flight)
+        {
+            if (Flights.ContainsKey(flight.FlightNumber))
+            {
+                return false;
+            }
+            else
+            {
+                Flights.Add(flight.FlightNumber, flight);
+                return true;
+            }
+        }
+        public double CalculateFees()
+        {
+            double totalFees = 0.0;
+            foreach (Flight flight in Flights.Values)
+            {
+                totalFees += flight.CalculateFees();
+            }
+
+            return totalFees;
+        }
+
+        public bool RemoveFlight(Flight flight)
+        {
+            if (Flights.ContainsKey(flight.FlightNumber))
+            {
+                Flights.Remove(flight.FlightNumber);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Airline: {Name, -20} (Code: {Code, -8}), Total Flights: {Flights.Count}";
+        }
     }
 }
